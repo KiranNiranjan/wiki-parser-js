e0d8f6a276b99d7e754d90e71185618694ecbb13
Kiran Niranjan <kiran@kike.co.in>
2017-03-12 18:44:54 +0530

    updated pip to python 3

--- startup.sh
+++ startup.sh
@@ -3,41 +3,41 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
     sudo apt-get install -y python3-pip
     apt update
-    sudo pip3 install wikiparser
+    sudo pip install wikiparser
     ;;
   'FreeBSD')
     OS='FreeBSD'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
-    sudo pip3 install wikiparser
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'CentOS')
     OS='CentOS'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
-    sudo pip3 install wikiparser
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'WindowsNT')
     OS='Windows'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
-    sudo pip3 install wikiparser
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'Darwin')
     OS='Mac'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
-    sudo pip3 install wikiparser
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'SunOS')
     OS='Solaris'
-    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-    python get-pip.py
-    sudo pip3 install wikiparser
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'AIX') ;;
   *) ;;
