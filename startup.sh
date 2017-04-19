3bdf53628180a08a878adca0b7da55e877514d0c
Kiran Niranjan <kiran@kike.co.in>
2017-04-20 00:04:37 +0530

    update startup for linux

--- startup.sh
+++ startup.sh
@@ -3,10 +3,9 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    sudo apt-get -y install python3-pip python-dev
-    sudo pip3 install wikiparser
-    python3 -V
-    pip3 -V
+    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install wikiparser
     ;;
   'FreeBSD')
     OS='FreeBSD'
