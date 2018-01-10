211e5635b24d4a0f16e28a2d117f5d8bc35e27c2
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2018-01-10 23:14:50 +0530

    Changed sh file

--- startup.sh
+++ startup.sh
@@ -1,42 +1,4 @@
 #!/usr/bin/env bash
-OS="`uname`"
-case $OS in
-  'Linux')
-    OS='Linux'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python -m pip install wikiparser
-    ;;
-  'FreeBSD')
-    OS='FreeBSD'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python3 -m pip install wikiparser
-    ;;
-  'CentOS')
-    OS='CentOS'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python3 -m pip install wikiparser
-    ;;
-  'WindowsNT')
-    OS='Windows'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python3 -m pip install wikiparser
-    ;;
-  'Darwin')
-    OS='Mac'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python3 -m pip install wikiparser
-    ;;
-  'SunOS')
-    OS='Solaris'
-    curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
-    sudo python get-pip.py
-    sudo python3 -m pip install wikiparser
-    ;;
-  'AIX') ;;
-  *) ;;
-esac
+curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
+python get-pip.py
+pip3 install wikiparser
