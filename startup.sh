0a9885dd9d699ffe308453f7f652888e1ffbc4dd
Kiran Niranjan <kiran@kike.co.in>
2018-01-11 01:17:13 +0530

    Added python3 install scripts

--- startup.sh
+++ startup.sh
@@ -2,5 +2,8 @@
 curl "https://bootstrap.pypa.io/get-pip.py | python3" -o "get-pip.py"
 python get-pip.py
 python --version
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python3
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 python3 --version
 pip install wikiparser
